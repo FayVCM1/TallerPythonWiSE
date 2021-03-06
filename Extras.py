a372ce5050cd0ddd276e7a39f087bb5cc9ba1d40
Fay Cervantes Marquez <48657993+FayVssCM@users.noreply.github.com>
2021-03-05 20:40:05 -0500

    Create Extras.py

---
+++ Extras.py
@@ -0,0 +1,103 @@
+# operadores logicos _________
+# print(5 + 5)
+# print(5 - 5)
+# print(5 * 5)
+# print(5 / 5)
+
+# raiz cuadrada____
+# import math
+# math.sqrt(5)
+
+# Respeta la jerarquía  
+# print(5 + 5 * 2)
+
+
+# variables______
+# num_int = 2
+# num_flot = 3.9
+# print (num)
+# print(num_int + num_flot)
+# num_con = int(num_flot)
+# num_con = float(num_int)
+# num_con = str(num_int)
+
+# resume un decimal en cierto numeros desps del punto
+# num_con = round(num_con, 2)
+
+# Caracteres_____________
+# nombre = "Fay"
+# nombre = 'Fay'
+# nombre2 = 'Fay Cervantes'
+# print (nombre)
+# print ( (nombre + ", ") * 3)
+# print (nombre2 + ", " + nombre)
+
+# solicitar un valor __________
+# a = input('Escriba un numero')
+# print(a)
+
+# num_bool = True / False
+# estudiante = True
+# trabaja = False
+# print (estudiante and not trabaja)
+
+# num1 = 5
+# num2 = 4
+# print (num1 == num2)
+# print (num1 != num2)
+# print (num1 > num2)
+# print (num1 < num2)
+# print (num1 >= num2)
+# print (num1 <= num2)
+
+# # concadenaciones _________
+# nombre = input("cual es tu nombre ")
+# # pone mayusculas
+# nombre1 = nombre.upper()
+# #pone mayusculas la primera letra
+# nombre2 = nombre.capitalize()
+# # quita espacio
+# nombre3 = nombre.strip()
+# # vuelve a minuscula
+# nombre4 = nombre.lower()
+# # remplaza letras
+# nombre5 = nombre.replace('a', 'o')
+# # letras por indice
+# nombre6 = nombre[0]
+# # contador de letras 
+# cont1 = str(len(nombre))
+# cont2 = str(len("hola"))
+# print (nombre1 + ", " + nombre2 + ", " + nombre3 + ", " + nombre4 + ", " + nombre5 + ", " + nombre6)
+# print(cont1 +  ", " + cont2)
+
+
+# slices_____________________
+# nombre = "Vanessa"
+# print(nombre)
+# print(nombre[0])
+# print(nombre[1])
+# print(nombre[0:3])
+# print(nombre[3:])
+# print(nombre[1:7])
+# print(nombre[1:7:2])
+# print(nombre[::])
+# print(nombre[1::3])
+# print(nombre[::-1])
+
+# RECUERDA DOS SALTOS DE LINEA DEBEN SEPARAR CADA FUNCION
+
+#bucles____________________
+# def ciclo1():
+      # constante en mayusculas no se modifican
+#     CONST = 1000
+#     contador = 0
+#     potencia = 2**contador
+#     while potencia < CONST:
+#         print("2 elevado a " + str(contador) + " : " + str(potencia))
+#         contador = contador + 1
+#         potencia = 2**contador
+
+
+# para iniciar el algoritmo, pytho comienza a leer desde aqui
+# if __name__ ==  '__main__':
+#     run()
